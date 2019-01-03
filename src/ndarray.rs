9c1bfdbca64760acb31cef3c338af177ed2f2495
SunDoge <384813529@qq.com>
2019-01-03 16:12:57 +0800

    Think about fixing type hint

--- src/ndarray.rs
+++ src/ndarray.rs
@@ -1,5 +1,5 @@
-use mxnet_sys::{MXNDArrayCreateNone, NDArrayHandle, MXNDArrayFree};
-use std::ptr;
+use mxnet_sys::{MXNDArrayCreateNone, MXNDArrayFree, MXNDArrayGetShape, NDArrayHandle};
+use std::{ptr, slice};
 
 pub enum DeviceType {
     CPU = 1,
@@ -16,10 +16,9 @@ impl Context {
     pub fn new(device_type: DeviceType, device_id: i32) -> Context {
         Context {
             device_type,
-            device_id
+            device_id,
         }
     }
-
 
     pub fn device_type(&self) -> &DeviceType {
         &self.device_type
@@ -50,6 +49,28 @@ impl NDArray {
         }
         NDArray { handle }
     }
+
+    pub fn size(&self) -> usize {
+        self.raw_shape().iter().fold(1, |acc, x| acc * *x)
+    }
+
+    pub fn shape(&self) -> Vec<usize> {
+        let ret_slice = self.raw_shape();
+        let mut ret = Vec::with_capacity(ret_slice.len());
+        for i in ret_slice {
+            ret.push(*i);
+        }
+        ret
+    }
+
+    fn raw_shape(&self) -> &[usize] {
+        let mut out_dim = 0;
+        let mut out_pdata = ptr::null();
+        unsafe {
+            MXNDArrayGetShape(self.handle, &mut out_dim, &mut out_pdata);
+            slice::from_raw_parts(out_pdata as *const usize, out_dim as usize)
+        }
+    }
 }
 
 impl Drop for NDArray {
@@ -60,13 +81,14 @@ impl Drop for NDArray {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn ndarray_create_none() {
-        let _ = NDArray::new();
+        let array = NDArray::new();
+        assert_eq!(array.shape(), vec![]);
+        assert_eq!(array.size(), 1);
     }
 }
