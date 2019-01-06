aa60edb91843513446600a4b958045ecae238ec3
SunDoge <384813529@qq.com>
2019-01-06 22:23:54 +0800

    no use

--- src/base.rs
+++ src/base.rs
@@ -1,18 +1,12 @@
 #[macro_export]
 macro_rules! check_call {
     ($mx_call:expr) => {
-        use mxnet_sys::MXGetLastError;
-        use std::ffi::CStr;
         unsafe {
             if $mx_call != 0 {
-                panic!(CStr::from_ptr(MXGetLastError()).to_str().unwrap());
+                panic!(std::ffi::CStr::from_ptr(mxnet_sys::MXGetLastError())
+                    .to_str()
+                    .unwrap());
             }
         }
     };
 }
-
-// fn check_call(ret: i32) {
-//     if ret != 0 {
-//         unsafe { panic!(CStr::from_ptr(MXGetLastError()).to_str().unwrap()) }
-//     }
-// }
